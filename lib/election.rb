ae93260df03bc5dc4c20d9ff73c45eddc24258fa
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2022-11-01 08:33:48 -0700

    feat: Add initial passing tests for Election

--- lib/election.rb
+++ lib/election.rb
@@ -1,2 +1,15 @@
+require './lib/candidate'
+
 class Election
+  attr_reader :year, :races
+
+  def initialize(year)
+    @year = year
+    @races = []
+  end
+
+  def add_race(race)
+    @races << race
+  end
 end
+
