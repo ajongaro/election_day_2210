f65a5792787bb411a6f999da52d8ba37bcc90ea5
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2022-11-01 08:57:06 -0700

    feat: Add complete iteration three

--- lib/election.rb
+++ lib/election.rb
@@ -11,5 +11,19 @@ class Election
   def add_race(race)
     @races << race
   end
+  
+  def candidates
+    @races.flat_map do |race|
+      race.candidates
+    end
+  end
+
+  def vote_counts
+    vote_register = {}
+    candidates.each do |candidate|
+      vote_register[candidate.name] = candidate.votes
+    end
+    vote_register
+  end
 end
 
--- spec/election_spec.rb
+++ spec/election_spec.rb
@@ -25,7 +25,24 @@ RSpec.describe Election do
   
   describe '#vote_counts' do
     it 'counts all the votes as a hash' do
-      # add stuff here
+      election.add_race(race1)
+      election.add_race(race2)
+
+      race1.register_candidate!( {name: "Diana D", party: :democrat} )
+      race1.register_candidate!( {name: "Roberto R", party: :republican} )
+      race2.register_candidate!( {name: "Diego D", party: :democrat} )
+      race2.register_candidate!( {name: "Rita R", party: :republican} )
+      race2.register_candidate!( {name: "Ida I", party: :independent} )
+
+      4.times {election.candidates[0].vote_for!}
+      1.times {election.candidates[1].vote_for!}
+      10.times {election.candidates[2].vote_for!}
+      6.times {election.candidates[3].vote_for!}
+      6.times {election.candidates[4].vote_for!}
+
+      expect(election.candidates.count).to eq(5)
+      expect(election.vote_counts).to eq({"Diana D"=>4, "Roberto R"=>1, "Diego D"=>10, "Rita R"=>6, "Ida I"=>6})
     end
+
   end
 end
