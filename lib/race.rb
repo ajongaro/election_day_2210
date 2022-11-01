1c458530e05bb04cf8f97162e6baad45107646a6
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2022-11-01 08:21:55 -0700

    feat: Add passing iteration 2

--- lib/race.rb
+++ lib/race.rb
@@ -1,2 +1,13 @@
+require './lib/candidate'
 class Race
+  attr_reader :office, :candidates
+
+  def initialize(office)
+    @office = office
+    @candidates = []
+  end
+
+  def register_candidate!(candidate)
+    @candidates << Candidate.new(candidate)
+  end
 end
