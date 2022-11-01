11b73cb9c06159200076b6ea049223511cb9b9ec
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2022-11-01 08:08:42 -0700

    test: Add tests for it1

---
+++ spec/candidate_spec.rb
@@ -0,0 +1,24 @@
+require './lib/candidate'
+
+RSpec.describe Candidate do
+  let(:diana) { Candidate.new({name: "Diana D", party: :democrat}) }
+  describe '#initialize' do
+    it 'exists' do
+      expect(diana).to be_a(Candidate)
+      expect(diana.name).to eq("Diana D")
+      expect(diana.votes).to eq(0)
+    end
+  end
+
+  describe '#vote_for!' do
+    it 'adds to the votes tally' do
+      expect(diana.votes).to eq(0)
+
+      diana.vote_for!
+      expect(diana.votes).to eq(1)
+
+      diana.vote_for!
+      expect(diana.votes).to eq(2)
+    end
+  end
+end
